81814d7ddbb7bc8f24d176dd9e26903125778445
Runchapol <43022331+Runchapol@users.noreply.github.com>
2021-05-02 17:51:47 +0700

    ดึงข้อมูลได้แล้วจ้าา

--- Web db/public/db.js
+++ Web db/public/db.js
@@ -60,7 +60,8 @@ function initUser(){
 	firebase.auth().onAuthStateChanged(function (user) {
 		//User Log in
 		if (user) {
-			console.log(user.email);
+			console.log(user.uid);
+			console.log(window.location.href);
 			document.getElementById("sign").textContent = "Sign Out";
 			document.getElementById("nosign").style.display = "none";
 			document.getElementById("insign").style.display = "block";
@@ -126,16 +127,87 @@ function resetpass(){
 	});
 }
 /**/
-let addList = () => {
-    var title = document.getElementById("int").value;
-    var txt = document.getElementById("txt").value;
-    const firebaseRef = firebase.database().ref("test");
-    firebaseRef.push({
-        title: title,
-        text: txt,
+function creOrder(){
+	var type = document.getElementById("type").value;
+    var price = document.getElementById("price").value;
+    var amount = document.getElementById("amount").value;
+    var bank = document.getElementById("bank").value;
+    var topic = document.getElementById("topic").value;
+    var detail = document.getElementById("detail").value;
+    if((amount == "") || (price == "") || (bank == "")){
+    	alert("Price, Amount and Bank must be fill");
+    	return;
+    }
+    firebase.auth().onAuthStateChanged(function (user){
+    	if(user){
+			const firebaseRef = firebase.database().ref("test");
+		    firebaseRef.push({
+		    	type: type,
+		        price: price,
+		        amount: amount,
+		        bank: bank,
+		        topic: topic,
+		        detail: detail,
+		        uid: user.uid,
+		        email: user.email,
+		    });
+		    alert("Create Order Complete!");
+		    window.location.href = "index.html";
+		}
+		else{
+			alert("Please Sign in");
+			window.location.href = "login.html";
+		}
     });
+}
 
-    alert("Add list Complete!");
-    document.getElementById("int").value = "";
-    document.getElementById("txt").value = "";
-};
+function update(){
+	var ref = firebase.database().ref("test");
+	ref.on('value', (snapshot) =>{
+		const data = snapshot.val();
+		console.log(data);
+		let key = Object.keys(data);
+		let chk;
+		let buyc = "";
+		let sellc = "";
+		console.log(key);
+		for (let i in key) {
+			console.log(key[i]);
+			chk = key[i];
+			if (data[chk].type == "buy"){
+				buyc += `<div class="row" style="border: 1px black solid;">
+							<div class="col-2">
+								<p>${data[chk].price}</p>
+							</div>
+							<div class="col-3">
+								<p>${data[chk].amount}</p>
+							</div>
+							<div class="col-2">
+								<p>${data[chk].bank}</p>
+							</div>
+							<div class="col-5">
+								<p>${data[chk].email}</p>
+							</div>
+						</div>`;
+			}
+			else{
+				sellc += `<div class="row" style="border: 1px black solid;">
+							<div class="col-2">
+								<p>${data[chk].price}</p>
+							</div>
+							<div class="col-3">
+								<p>${data[chk].amount}</p>
+							</div>
+							<div class="col-2">
+								<p>${data[chk].bank}</p>
+							</div>
+							<div class="col-5">
+								<p>${data[chk].email}</p>
+							</div>
+						</div>`;
+			}
+		}
+		document.getElementById("buyc").innerHTML = buyc;
+		document.getElementById("sellc").innerHTML = sellc;
+	});
+}
